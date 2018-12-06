ffc0e12a956654b2f16d5174934e3410f66af0b6
Marvnc93 <42253000+Marvnc93@users.noreply.github.com>
2018-12-06 14:38:02 +0800

    First succesful connection to database

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (RED) {
-
-    RED.nodes.createNode(this, config);
-
-    function connection() {
+    var CosmosClient = require('@azure/cosmos').CosmosClient;
+    'use strict';
+    // Handling of the node-red configuration input
+    function connection(config) {
         RED.nodes.createNode(this, config);
         console.log("Connection");
         var node = this;
@@ -10,23 +10,81 @@ module.exports = function (RED) {
             {
                 endpoint: node.credentials.endpoint,
                 key: node.credentials.key,
-                database: node.database,
-                collection: node.collection
+                database: config.database,
+                collection: config.collection
             };
-        RED.nodes.registerType('Connection', connection);
+        //this.connection = client;
     };
-   
-    function cosmos() {
+    RED.nodes.registerType('Connection', connection,
+        {
+            credentials:
+            {
+                endpoint: { type: 'text' },
+                key: { type: 'password' }
+            }
+        });
+    // Create the connection to the cosmos db and post the output
+    function cosmos(config) {
         RED.nodes.createNode(this, config);
-        console.log("Cosmos")
-    };
-    
-    node.on('input', function (msg) {
-        //msg.payload = msg.payload.toLowerCase();
-        console.log(node.config)
-        msg.payload = "TEST";
-        node.send(msg);
-    });
+        var connection = RED.nodes.getNode(config.azrcon)
+        console.log("Cosmos");
+        var node =this;
+        this.config = connection.config;
+        //this.connection = connection.connection;
 
+        node.on('input', function (msg) {
+            
+            const endpoint = String(node.config.endpoint);
+            const masterKey = String(node.config.key);
+            const databaseId = String(node.config.database);
+
+            //const masterKey = String("This is a wrong key");
+            // const endpoint = "https://mczdb.documents.azure.com:443/";
+            // const masterKey = "hE3lHT6PeBaQJlYIPXY4e4AYwSbhK9qJ4uNwHWwuuLU7hxyf4FYcwcwC76oXJiCj4dNNas5rxyC7gYk0cPMeOA==";
+            // const databaseId = "db1";
+            
+            const containerId = String(node.config.collection);
+            console.log("Endpoint: "+endpoint);
+            console.log("Key: "+masterKey);
+            console.log("Database: "+databaseId);
+            console.log("Container: "+containerId);
+            const client = new CosmosClient({ endpoint: endpoint, auth: {masterKey:masterKey}});
+
+            // async function createContainer() {
+            //     const { container } = await client.database(databaseId).containers.createIfNotExists({ id: "NewCont" });
+            //     console.log(`Created container`);
+            // }
+            // createContainer();
+            //const container = client.database(databaseId).containers.createIfNotExists({ id: "NewCont" });
+
+
+
+            async function queryContainer() {
+                console.log(`Querying container:\n${containerId}`);
+                
+                // query to return all children in a family
+                const querySpec = {
+                    query: "SELECT VALUE c.id FROM c WHERE c.Temperature > @Temp",
+                    parameters: [
+                        {
+                            name: "@Temp",
+                            value: 23
+                        }
+                    ]
+                };
+                
+                const {result:results} = await client.database(databaseId).container(containerId).items.query(querySpec).toArray();
+                console.log(`Got result: ${results}`);
+                for (var queryResult of results) {
+                    let resultString = JSON.stringify(queryResult);
+                    console.log(`\tQuery returned ${resultString}\n`);
+                    
+                }
+                node.send(results.join(', '));
+            };
+            queryContainer();
+            //node.send(msg);
+        });
+    }
     RED.nodes.registerType('Cosmos', cosmos);
 };

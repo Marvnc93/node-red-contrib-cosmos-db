40a792c301309df790fc3e9969f5759a0446a56f
Marvin <marvnc7@gmail.com>
2019-01-10 09:18:17 +0800

    First version

--- index.js
+++ index.js
@@ -24,44 +24,29 @@ module.exports = function (RED) {
             }
         });
     // Create the connection to the cosmos db and post the output
-    function cosmos(config) {
+    function query(config) {
         RED.nodes.createNode(this, config);
         var connection = RED.nodes.getNode(config.azrcon)
-        console.log("Cosmos");
-        var node =this;
+        var node = this;
         this.config = connection.config;
+        this.query = config.query;
         //this.connection = connection.connection;
 
         node.on('input', function (msg) {
-            
+
             const endpoint = String(node.config.endpoint);
             const masterKey = String(node.config.key);
             const databaseId = String(node.config.database);
-
-            //const masterKey = String("This is a wrong key");
-            // const endpoint = "https://mczdb.documents.azure.com:443/";
-            // const masterKey = "hE3lHT6PeBaQJlYIPXY4e4AYwSbhK9qJ4uNwHWwuuLU7hxyf4FYcwcwC76oXJiCj4dNNas5rxyC7gYk0cPMeOA==";
-            // const databaseId = "db1";
-            
             const containerId = String(node.config.collection);
-            console.log("Endpoint: "+endpoint);
-            console.log("Key: "+masterKey);
-            console.log("Database: "+databaseId);
-            console.log("Container: "+containerId);
-            const client = new CosmosClient({ endpoint: endpoint, auth: {masterKey:masterKey}});
 
-            // async function createContainer() {
-            //     const { container } = await client.database(databaseId).containers.createIfNotExists({ id: "NewCont" });
-            //     console.log(`Created container`);
-            // }
-            // createContainer();
-            //const container = client.database(databaseId).containers.createIfNotExists({ id: "NewCont" });
-
-
-
+            // console.log("Endpoint: "+endpoint);
+            // console.log("Key: "+masterKey);
+            // console.log("Database: "+databaseId);
+            // console.log("Container: "+containerId);
+            const client = new CosmosClient({ endpoint: endpoint, auth: { masterKey: masterKey } });
             async function queryContainer() {
                 console.log(`Querying container:\n${containerId}`);
-                
+
                 // query to return all children in a family
                 const querySpec = {
                     query: "SELECT VALUE c.id FROM c WHERE c.Temperature > @Temp",
@@ -72,19 +57,57 @@ module.exports = function (RED) {
                         }
                     ]
                 };
-                
-                const {result:results} = await client.database(databaseId).container(containerId).items.query(querySpec).toArray();
+
+                const { result: results } = await client.database(databaseId).container(containerId).items.query(node.query).toArray();
                 console.log(`Got result: ${results}`);
                 for (var queryResult of results) {
                     let resultString = JSON.stringify(queryResult);
-                    console.log(`\tQuery returned ${resultString}\n`);
-                    
                 }
-                node.send(results.join(', '));
+                node.send({ 'payload': results, 'query': node.query });
             };
             queryContainer();
-            //node.send(msg);
+
         });
     }
-    RED.nodes.registerType('Cosmos', cosmos);
+    RED.nodes.registerType('Query', query);
+
+    function addentry(config) {
+        RED.nodes.createNode(this, config);
+        var connection = RED.nodes.getNode(config.azrcon)
+        var node = this;
+        this.config = connection.config;
+
+        node.on('input', function (msg) {
+
+            const endpoint = String(node.config.endpoint);
+            const masterKey = String(node.config.key);
+            const databaseId = String(node.config.database);
+            const containerId = String(node.config.collection);
+            const HttpStatusCodes = {NOTFOUND: 404};
+            const client = new CosmosClient({ endpoint: endpoint, auth: { masterKey: masterKey } });
+            async function createItem(itemBody) {
+                try {
+                    // read the item to see if it exists
+                    const { item } = await client.database(databaseId).container(containerId).item(itemBody.id).read();
+                    console.log(`Item with family id ${itemBody.id} already exists\n`);
+                }
+                catch (error) {
+                    // create the family item if it does not exist
+                    if (error.code === HttpStatusCodes.NOTFOUND) {
+                        const { item } = await client.database(databaseId).container(containerId).items.create(itemBody);
+                        console.log(`Created family item with id:\n${itemBody.id}\n`);
+                    } else {
+                        throw error;
+                    }
+                }
+            };
+            console.log(msg.payload);
+            msg.payload.forEach(function(element,index){
+                createItem(element);
+            });
+            
+            //createItem(msg.payload);
+        });
+    }
+    RED.nodes.registerType('Push', addentry)
 };
